9147c40dfb54d01fd560f442db0f980be18c0ce5
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-08-03 19:18:48 +0530

    Correct giveroles command to include displayName

--- src/commands/giveroles.ts
+++ src/commands/giveroles.ts
@@ -21,16 +21,38 @@ export default class GiveRolesCommand implements IBotCommand {
             answer.message.channel.send(`Fetching all members`);
             const allMembers = await this._bot.guild.members.fetch();
             answer.message.channel.send(`Fetched all members`);
-            //for (const mem of allMembers) {
-            //    answer.message.channel.send(`${mem}`);
+            answer.message.channel.send(`Here are all members:`);
+            //answer.message.channel.send(JSON.stringify(allMembers));
+            //for (const mem of allMembers) { //mem is the index of arrray
+                //answer.message.channel.send(`${mem}`); //displays user # and tagged handle
+              //  answer.message.channel.send(allMembers[mem].displayName);
+                
             //}
+            const newarray = allMembers.array()
+            answer.message.channel.send(newarray.length);
+
+
+
+            //for (const mtag of this._bot.userTags) { //print all user tags in user_tags.txt
+              //  answer.message.channel.send(`${mtag}`);
+                
+            //}
+
             answer.message.channel.send(`Role to assign:`);
             answer.message.channel.send(this._bot.role.id);
             const members = allMembers
-                //.filter((m) => this._bot.userTags.has(m.user.tag))
-                //.filter((m) => !m.roles.cache.has(this._bot.role.id))
+                .filter((m) => this._bot.userTags.has(m.displayName))
+                .filter((m) => !m.roles.cache.has(this._bot.role.id))
                 .array();
+
+
             answer.message.channel.send(`Assigning role to ${members.length} members`);
+            
+
+            for (const mem of members) { //print member array
+                answer.message.channel.send(`${mem}`);
+            }
+
             const addRole = (member) => {
                 answer.message.channel.send(member); 
                 return member.roles.add(this._bot.role);
